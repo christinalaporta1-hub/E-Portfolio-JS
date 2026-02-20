bea6a53b99748886bbd9b18f523b0db16f83c2d5
christinalaporta1-hub <christinalaporta1@gmail.com>
2026-02-20 14:24:06 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//template_h1fwjyj
+//service_76lazhe
+//MqYsCl6uQeR9iL2dv
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+
+   emailjs
+   .sendForm(
+  'service_76lazhe', 
+  'template_h1fwjyj', 
+   event.target, 
+  'MqYsCl6uQeR9iL2dv')
+  .then (() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+   })
+   .catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert(
+        "The email service is temporarily unavailable. Please contact me directly at ChristinaLaporta1@gmail.com"
+    );
+   })
+
+
+loading.classList += " modal__overlay--visible"
+setTimeout(() => {
+
+    console.log ('it worked 1')
+}, 1000);
+
+}
