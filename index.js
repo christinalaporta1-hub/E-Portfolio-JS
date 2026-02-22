38bb8958935124b2fb75e34c756487883c1796b6
christinalaporta1-hub <christinalaporta1@gmail.com>
2026-02-21 19:26:47 -0500

    done

--- index.js
+++ index.js
@@ -3,6 +3,19 @@
 //MqYsCl6uQeR9iL2dv
 let isModalOpen = false;
 let isContrastToggled = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
 
 function toggleContrast() {
     isContrastToggled = !isContrastToggled;
